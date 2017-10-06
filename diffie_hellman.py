d6ad9144082845be781663dd3fa938572dd62ba5
Michael Asper <asper@utexas.edu>
2017-10-05 19:49:52 -0500

    simple diffie hellman implementation

---
+++ diffie_hellman.py
@@ -0,0 +1,39 @@
+from math import sqrt, ceil, gcd
+from sympy.ntheory import is_primitive_root, generate
+from secrets import randbits
+
+class Diffie(object):
+    
+    def __init__(self, key_size, secret, key=0, generate=True):
+        self.key_size = key_size
+        self.sys_seed = randbits(key_size)
+        if(generate):
+            self.generate_public_secret()
+        else:
+            self.prime = key[0]
+            self.g = key[1]
+        self.secret = secret
+
+    def get_common(self):
+        return (self.prime, self.g)
+
+    def generate_prime(self):
+        self.prime = generate.nextprime(self.sys_seed)
+
+    def generate_public_secret(self):
+        self.generate_prime()
+        self.find_prime_root_mod()
+        return self.prime, self.g
+
+    def find_prime_root_mod(self):
+        for x in range(2,self.prime):
+            if is_primitive_root(x,self.prime):
+                self.g = x
+                break
+    
+    def compute_shared(self):
+        return pow(self.g, self.secret, self.prime)
+
+    def get_shared_secret(self, shared):
+        return pow(shared, self.secret, self.prime)
+        
---
+++ main.py
@@ -0,0 +1,14 @@
+import diffie_hellman
+
+def main():
+    alice = diffie_hellman.Diffie(128, 53)
+    key = alice.get_common()
+    bob = diffie_hellman.Diffie(128, 29, key, False)
+    shared_alice = alice.compute_shared()
+    shared_bob = bob.compute_shared()
+    print(alice.get_shared_secret(shared_bob))
+    print(bob.get_shared_secret(shared_alice))
+
+
+if __name__ == "__main__":
+    main()
